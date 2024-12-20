2991081b33f6c4fbafbe82d04f8b7d065f4d75e1
Matei Cloteaux <matei.cloteaux@gmail.com>
2024-12-20 01:02:16 -0500

    Sandbox progress

--- js/deduce.js
+++ js/deduce.js
@@ -1,3 +1,8 @@
+//
+// TODO: . does not style
+//
+
+
 
 require.config({ paths: { vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.29.1/min/vs/' } });
 
@@ -49,7 +54,7 @@ require(['vs/editor/editor.main'], function () {
 
 
         // we include these common regular expressions
-        symbols: /(=|>|<|!|~|\?|:|&|\||\+|-|\^|%|\)|\(|\]|\[|o|0|≠|⊆|≤|≥|∈|∪|∅|.)/,
+        symbols: /(=|>|<|!|~|\?|:|&|\||\+|-|\^|%|\)|\(|\]|\[|o|0|≠|⊆|≤|≥|∈|∪|∅|\.)/,
 
         specialSymbols: /((?<!\/|\*)\/(?!\/|\*)|(?<!\/)\*(?!\/))/,
 
@@ -70,7 +75,7 @@ require(['vs/editor/editor.main'], function () {
                 // delimiters and operators
                 [/[{}()\[\],]/, 'brackets'],
                 // [/[<>](?!@symbols)/, '@brackets'],
-                [/[*=><!~?:&|+^%≤≥⇔∘∪∩⊆∈⨄∅-]+/, {
+                [/[.@*=><!~?:&|+^%≤≥⇔∘∪∩⊆∈⨄∅-]+/, {
                     cases: {
                         '@primSymbols': 'primitive',
                         '@operators': 'operator',
@@ -123,26 +128,98 @@ require(['vs/editor/editor.main'], function () {
 
     const editor = monaco.editor.create(document.getElementById('container'), {
         theme: 'deduce-dark',
-        value: "// Enter deduce code here",
-        language: 'deduce'
+        value: "// Enter deduce code here\n\n\n",
+        language: 'deduce',
+        automaticLayout: true
     });
 
     const butt = document.getElementById("submit")
     const output = document.getElementById("code-output")
 
-    const deduceServerURL = "http://127.0.0.1:12347/deduce"
+    const deduceServerURL = "https://deduce.vercel.app/deduce"
 
-    function do_thing(code) {
-        output.innerHTML = "Sending..."
+    function prepare_output(out, is_err = false, re_sp = true) {
+        out = out.replaceAll("\n", "<br>")
+        out = out.replaceAll("\t", "    ")
+        out = re_sp ? out.replaceAll(" ", "&nbsp;") : out
+        return is_err ? `<span class="error">${out}</span>` : out
+    }
+
+    function send_deduce(code) {
+        output.innerHTML = '<div class="loader">Deducing<span></span></div>'
         fetch(deduceServerURL, {
             method: "POST",
             body: code
-        }
-        )
-            .then(res => res.text())
-            .then(data => output.innerHTML = data.replaceAll("\n", "<br>").replace("test.pf", "input"))
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.text()
+                }
+                throw new Error('')
+            })
+            .then(data => output.innerHTML = prepare_output(data))
+            .catch(error => output.innerHTML = prepare_output('Something went wrong internally.\nIf this error persists please reach us at <a href="mailto:jsiek@iu.edu">jsiek@iu.edu</a>.',
+                is_err = true,
+                re_sp = false))
     }
 
 
-    butt.onclick = () => do_thing(editor.getValue())
+    butt.onclick = () => send_deduce(editor.getValue())
 });
+
+
+
+const resizerNS = document.querySelector('#resizer-ns');
+const resizerEW = document.querySelector('#resizer-ew');
+const wrapper = document.querySelector('.sandbox');
+const container = wrapper.querySelector('.in');
+let isHandlerDraggingNS = false;
+let isHandlerDraggingEW = false;
+
+document.addEventListener('mousedown', function (e) {
+    // If mousedown event is fired from .handler, toggle flag to true
+    if (e.target === resizerNS) {
+        isHandlerDraggingNS = true;
+    }
+    if (e.target === resizerEW) {
+        isHandlerDraggingEW = true;
+    }
+});
+
+document.addEventListener('mousemove', function (e) {
+    // Don't do anything if dragging flag is false
+    if (!isHandlerDraggingNS && !isHandlerDraggingEW) {
+        return false;
+    }
+
+    // Get offset
+    var containerOffsetLeft = wrapper.offsetLeft;
+    var containerOffsetTop = wrapper.offsetTop;
+
+    // Get x-coordinate of pointer relative to container
+    var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+    var pointerRelativeYpos = e.clientY - containerOffsetTop;
+
+    // Arbitrary minimum width set on box A, otherwise its inner content will collapse to width of 0
+    var containerMin = 60;
+
+    // Resize box A
+    // * 8px is the left/right spacing between .handler and its inner pseudo-element
+    // * Set flex-grow to 0 to prevent it from growing
+    if (isHandlerDraggingEW) {
+        container.style.width = (Math.max(containerMin, pointerRelativeXpos - 8)) + 'px';
+        container.style.flexGrow = 0;
+    } else if (isHandlerDraggingNS) {
+
+        console.log("heheheha")
+        container.style.height = (Math.max(containerMin, pointerRelativeYpos - 8)) + 'px';
+        // container.style.flexGrow = 0;
+    }
+});
+
+document.addEventListener('mouseup', function (e) {
+    // Turn off dragging flag when user mouse is up
+    isHandlerDraggingNS = false;
+    isHandlerDraggingEW = false;
+});
+
